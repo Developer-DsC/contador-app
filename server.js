92dc8a6cc1fc8e45346569283774b5be7d8edb51
Dev-DsC <daltonsteve22@gmail.com>
2025-04-14 17:12:27 -0500

    Actualización para despliegue en Railway

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Aquí debes poner el nombre exacto de tu app (contador-app)
-app.use(express.static(path.join(__dirname, 'dist/contador-app')));
+app.use(express.static(__dirname + '/dist/contador-app'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/contador-app/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/contador-app/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+    console.log(`Server running on port ${port}`);
 });
