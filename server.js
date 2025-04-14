4216401d6619068609d755e7c932e6e3e003b666
Dev-DsC <daltonsteve22@gmail.com>
2025-04-14 17:26:28 -0500

    Actualización para despliegue en Railway

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/contador-app'));
+// Servir archivos estáticos de la carpeta dist/contador-app
+app.use(express.static(path.join(__dirname, 'dist', 'contador-app')));
 
+// Redirigir todas las solicitudes a index.html
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/contador-app/index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'contador-app', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
