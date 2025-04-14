b2c8bec410b94ba2c4012a34f7b4cbd75667c03d
Dev-DsC <daltonsteve22@gmail.com>
2025-04-14 18:02:46 -0500

    Registro de 1 IP

--- server.js
+++ server.js
@@ -2,22 +2,25 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Middleware para registrar las IPs
+// Middleware para registrar las IPs solo una vez por sesión
 app.use((req, res, next) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(`Usuario accediendo desde la IP: ${ip}`);
-    next(); // Asegúrate de llamar a 'next()' para que continúe la ejecución
+    if (!req.session) {
+        req.session = {}; // Asegúrate de que la sesión esté inicializada
+    }
+    if (!req.session.ip) {
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log(`Usuario accediendo desde la IP: ${ip}`);
+        req.session.ip = ip; // Guarda la IP en la sesión para evitar registros repetidos
+    }
+    next();
 });
 
-// Configura Express para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'dist/contador-app/browser')));
 
-// Define la ruta para todas las demás peticiones
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/contador-app/browser/index.html'));
 });
 
-// Inicia el servidor en el puerto especificado
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
